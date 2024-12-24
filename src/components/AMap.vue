febe4a800a76f0aac88e06a78a0a0c27f980b9f1
HavocZhang <havoc_zhang@yeah.net>
2024-12-24 22:36:35 +0800

    feat: 增加不同状态

--- src/components/AMap.vue
+++ src/components/AMap.vue
@@ -18,7 +18,21 @@ async function initMap() {
     center: [116.4, 39.92],
     resizeEnable: true,
   });
-  const mockData = [0, 1, 2, 3, 4, 5, 6];
+  const mockData = [
+    { id: 0, type: 1 },
+    { id: 1, type: 1 },
+    { id: 2, type: 2 },
+    { id: 3, type: 2 },
+    { id: 4, type: 2 },
+  ];
+  const typeImage = {
+    1: "/1.png",
+    2: "/2.png",
+  };
+  const typeImageClick = {
+    1: "/1-c.png",
+    2: "/2-c.png",
+  };
   const viaMarkerList: any[] = [];
   mockData.forEach((item, index) => {
     // 以 icon URL 的形式创建一个途经点
@@ -26,48 +40,48 @@ async function initMap() {
       position: new AMap.LngLat(116.38 + index, 39.92 + index),
       // 将一张图片的地址设置为 icon
       icon: new AMap.Icon({
-        // 图标尺寸
-        size: new AMap.Size(25, 34),
-        // 图标的取图地址
-        image:
-          "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
-        // 图标所用图片大小
-        imageSize: new AMap.Size(135, 40),
-        // 图标取图偏移量
-        imageOffset: new AMap.Pixel(-9, -3),
+        image: (typeImage as any)[item.type],
       }),
       // 设置了 icon 以后，设置 icon 的偏移量，以 icon 的 [center bottom] 为原点
       offset: new AMap.Pixel(-13, -30),
       //用户自定义属性，支持JavaScript API任意数据类型，如Marker的id等
       extData: {
-        id: item,
+        id: item.id,
+        type: item.type,
       },
     });
     viaMarker.on("click", (e: any) => {
       let id = e.target.getExtData().id;
+      let type = e.target.getExtData().type;
       viaMarkerList.forEach((markerItem) => {
         let markerId = markerItem.getExtData().id;
         if (id === markerId) {
           //相同放大
           e.target.setIcon(
             new AMap.Icon({
-              size: new AMap.Size(25, 34),
-              image:
-                "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
-              imageSize: new AMap.Size(135, 40),
-              imageOffset: new AMap.Pixel(-95, -3),
+              image: (typeImageClick as any)[type],
             })
           );
+          e.target.setExtData({
+            id: id,
+            type: type,
+            state: 1,
+          });
         } else {
           //不同复原
-          markerItem.setIcon(
-            new AMap.Icon({
-              size: new AMap.Size(25, 34),
-              image: markerItem.getIcon()._opts.image,
-              imageSize: new AMap.Size(135, 40),
-              imageOffset: new AMap.Pixel(-9, -3),
-            })
-          );
+          if (markerItem.getExtData().state === 1) {
+            markerItem.setIcon(
+              new AMap.Icon({
+                image: (typeImage as any)[markerItem.getExtData().type],
+              })
+            );
+          } else {
+            markerItem.setIcon(
+              new AMap.Icon({
+                image: markerItem.getIcon()._opts.image,
+              })
+            );
+          }
         }
       });
     });
